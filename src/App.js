d29c6a4ad214d29e7ef9545eb9167b0e7d88bc9b
Yeshi Tsering Gangchen <yestsering@gmail.com>
2021-05-09 12:54:27 +0200

    training component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 // Components
 import Navbar from "../src/components/navbar/Navbar.component";
-import Header from "../src/components/header/Header.component";
+import Header from "../src/components/header/Header.component"
 import About from "../src/pages/about/About.component";
 import Skills from "../src/pages/skills/Skills.component";
+import Training from "../src/pages/training/Training.component"
 
 
 
@@ -16,23 +17,9 @@ function App() {
     <>
       <Navbar />
       <Header />
-      {/* <div className="parallex">
-        <Parallax
-          blur={10}
-          bgImage={require("./assets/img/parallex/background.jpg")}
-          bgImageAlt=""
-          strength={200}
-        >
-          <Container className="container-box rounded">
-            <Fade left>
-              <About />
-            </Fade>
-          </Container>
-        </Parallax>
-      </div> */}
       <About />
-      
       <Skills />
+      <Training />
     </>
 
   );
---
+++ src/pages/training/Training.component.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import "../training/Training.style.css";
+
+import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Card from "react-bootstrap/Card";
+import L_KONEXIO from "../../assets/img/training/konexio.png";
+
+// npm package
+import Tilt from 'react-tilt';
+import Fade from 'react-reveal/Fade';
+import Slide from 'react-reveal/Slide';
+
+
+const Experience = () => {
+    return (
+        <div>
+            <Container className="container-box rounded">
+                <Slide bottom duration={500}>
+                    <hr />
+                        <h1 className="pt-3 text-center font-details-b pb-3">Experience</h1>
+                        <Jumbotron className="jumbotron-style">
+                            <Container >
+                                <Tilt options={{ max: 25 }}>
+                                    <Card>
+                                        <Card.Header as="h5" className="d-flex justify-content-center flex-wrap">
+                                            <Card.Img variant="top" className="img-resize" src={L_KONEXIO} alt="Accenture logo" />
+                                        </Card.Header>
+                                        <Card.Body className="d-flex justify-content-center flex-column">
+                                            <div>
+                                                <Card.Title className="text-center">
+                                                    <strong className="body-title-style ">Web &amp; Mobile Developer(fullstack)</strong>
+                                                </Card.Title>
+                                            </div>
+                                            <div>
+                                                <Card.Text className="text-left style">
+                                                    <ul className="text-left">
+                                                        <li><strong>Technology:</strong> JavaScript, React JS, React Native, Node JS, Express, MongoDB, MS-SQL, HTML, CSS, Bootstrap</li>
+
+                                                        <li><strong>Duration:</strong> Nov 2020 - Present</li>
+                                                        <li><strong> Description </strong> Nov 2020 - Present</li>
+                                                    </ul>
+                                                </Card.Text>
+                                            </div>
+                                        </Card.Body>
+                                    </Card>
+                                </Tilt>
+                            </Container>
+                        </Jumbotron>
+                </Slide>
+            </Container>
+        </div>
+    )
+}
+
+export default Experience
