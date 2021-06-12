8dbf3cf3cbb23838bc6e34d481099e4ba804e0c7
Yeshi Tsering Gangchen <yestsering@gmail.com>
2021-06-12 11:36:14 +0200

    Add MERN login Project

--- src/components/projects/Portfolio-data.js
+++ src/components/projects/Portfolio-data.js
@@ -1,10 +1,30 @@
 import L_Cuizine from "../../assets/img/projects/cuizine.png";
 import L_Quiz from "../../assets/img/projects/quiz.png";
 import L_Pokedex from "../../assets/img/projects/pokedex.png";
+import L_MERNLogin from "../../assets/img/projects/Mern-profile-project.png";
 
 
 export const timelineElements = {
     projects: [
+        {
+            id: 7,
+            title: "MERN Login, Signup & profile",
+            technologiesUsed: [
+                "Node JS",
+                "Express",
+                "Javascript (React JS)",
+                "MongoDB",
+                "Bootstrap",
+                "CSS", 
+                "HTML"
+                ],
+            description: "Site created as learning purpose, with used of MERN Technology, To store the Database, it used the MongoDB Atlas to store data online, For the Backend it Node JS with Express & Frontend, it Used Javascript framework React-JS with the help of Bootstrap. It is completed Fullstack project with responsive and Dynamic web page.",
+            buttontext: "View the MERN fullstack Projects",
+            date: "June 2021",
+            icon: "work",
+            image: L_MERNLogin,
+            view:   "https://mernloginprofile.herokuapp.com/signup",
+        },
         
         {
             id: 8,
